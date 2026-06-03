206c2b2cdded40d470e1914eb29f9f60ae689966
Braulio <braulio.diez@gmail.com>
2026-06-03 17:37:16 +0200

    update

--- 03-tan-stack-start/10-better-auth/src/routes/(auth)/intranet/route.tsx
+++ 03-tan-stack-start/10-better-auth/src/routes/(auth)/intranet/route.tsx
@@ -1,5 +1,6 @@
 import { getUserSession } from "#/core/user-session";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import IntranetHeader from "@/components/IntranetHeader";
 
 export const Route = createFileRoute("/(auth)/intranet")({
   loader: () => getUserSession(),
@@ -10,8 +11,10 @@ function RouteComponent() {
   const session = Route.useLoaderData();
   return (
     <div>
-      <h1>Intranet - Welcome {session.user.name}</h1>
-      <Outlet />
+      <IntranetHeader userName={session.user.name} />
+      <main className="page-wrap py-6">
+        <Outlet />
+      </main>
     </div>
   );
 }
